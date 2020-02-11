edde3c817c1765efc223220d61982728d25336d8
ecmnet <ecm@gmx.de>
2020-02-11 15:43:10 +0100

    SLAM quality added

--- mavodometry/src/main/java/com/comino/mavodometry/estimators/MAVR200DepthEstimator.java
+++ mavodometry/src/main/java/com/comino/mavodometry/estimators/MAVR200DepthEstimator.java
@@ -27,6 +27,10 @@ import georegression.struct.se.Se3_F64;
 
 public class MAVR200DepthEstimator {
 
+	private static final float COLLISION_WARNING_DISTANCE  = 1f;
+	private static final float MAX_DISTANCE                = 10f;
+	private static final int   CYCLE                       = 100;
+
 	private StreamRealSenseVisDepth 	realsense	= null;
 	private RealSenseInfo               info        = null;
 	private DataModel                   model       = null;
@@ -42,7 +46,7 @@ public class MAVR200DepthEstimator {
 	private DepthSparse3D<GrayU16> pixel2Body = null;
 	private Se3_F64                to_ned     = new Se3_F64();
 
-	private  IMAVMapper                mapper = null;
+	private double     	current_min_distance  = 0.0f;
 
 	public <T> MAVR200DepthEstimator(IMAVMSPController control, MSPConfig config, int width, int height,  IMAVMapper mapper) {
 		this(control,config,width,height, mapper,null);
@@ -51,18 +55,21 @@ public class MAVR200DepthEstimator {
 	public <T> MAVR200DepthEstimator(IMAVMSPController control, MSPConfig config, int width, int height,
 			 IMAVMapper mapper, IVisualStreamHandler<Planar<GrayU8>> stream) {
 
-		this.info = new RealSenseInfo(width,height, RealSenseInfo.MODE_RGB);
-		this.realsense = new StreamRealSenseVisDepth(0,info);
 
 		this.width   = width;
 		this.height  = height;
 		this.model   = control.getCurrentModel();
-		this.mapper  = mapper;
 
 		this.pixel2Body = new DepthSparse3D.I<GrayU16>(1e-3);
 
+		this.info = new RealSenseInfo(width,height, RealSenseInfo.MODE_RGB);
+
+		this.realsense = new StreamRealSenseVisDepth(0,info);
+
 		PointToPixelTransform_F32 visToDepth_pixel = new PointToPixelTransform_F32(new DoNothing2Transform2_F32());
 		this.pixel2Body.configure(narrow(realsense.getIntrinsics()),visToDepth_pixel);
+
+
 
 		if(stream!=null) {
 			stream.registerOverlayListener(ctx -> {
@@ -77,8 +84,11 @@ public class MAVR200DepthEstimator {
 
 			int x; int y; int depth_z; int raw_z;
 
-			Point3D_F64 body_pt =  null;
+			Point3D_F64 raw_pt  =  null;
+			Point3D_F64 body_pt =  new Point3D_F64();
 			Point3D_F64 ned_pt  =  new Point3D_F64();
+
+			long tms = 0; int quality = 0;
 
 			@Override
 			public void process(Planar<GrayU8> rgb, GrayU16 depth, long timeRgb, long timeDepth) {
@@ -88,8 +98,16 @@ public class MAVR200DepthEstimator {
 					stream.addToStream(rgb, model, timeDepth);
 				}
 
+				// limit processing to 10Hz
+				if((System.currentTimeMillis() - tms) < CYCLE)
+					return;
+				tms = System.currentTimeMillis();
+
 				pixel2Body.setDepthImage(depth);
 				MSP3DUtils.convertModelToSe3_F64(model, to_ned);
+
+				current_min_distance = Double.MAX_VALUE;
+				quality = 0;
 
 				// Read minimum depth in a band around BASE
 				for( x = 0; x < depth.width; x++ ) {
@@ -109,21 +127,47 @@ public class MAVR200DepthEstimator {
 						// transform to world coordinates in body frame
 						pixel2Body.process(x, y);
 						pixel2Body.getWorldPt();
-						body_pt = pixel2Body.getWorldPt();
+						raw_pt = pixel2Body.getWorldPt();
 
-						System.out.println(body_pt);
+					//	System.out.println(x+": "+raw_pt);
 
-						// rotate in NED frame
-						GeometryMath_F64.mult(to_ned.R, body_pt, ned_pt );
-						ned_pt.plusIP(to_ned.T);
+						quality++;
 
-						// put into map
-						if(mapper!=null)
-							mapper.update(ned_pt);
+						// Maximum distance handled
+						if(raw_pt.z > MAX_DISTANCE)
+							continue;
 
+						body_pt.set(raw_pt.z, raw_pt.x, raw_pt.y);
+
+
+						// get min obstacle distance
+						if(body_pt.x < current_min_distance)
+							current_min_distance = body_pt.x;
+
+						// put into map if map available
+						if(mapper!=null) {
+
+							// rotate in NED frame
+							GeometryMath_F64.mult(to_ned.R, body_pt, ned_pt );
+							ned_pt.plusIP(to_ned.T);
+
+							mapper.update(model.state.l_x, model.state.l_y,ned_pt);
+
+							// only set SLAM to valid if mapping is available
+							model.slam.tms = model.sys.getSynchronizedPX4Time_us();
+
+						}
 					}
 
 				}
+
+				model.slam.quality = quality * 100 / width;
+
+				if(current_min_distance<COLLISION_WARNING_DISTANCE)
+					model.slam.dm = (float)current_min_distance;
+				else
+					model.slam.dm = Float.NaN;
+
 			}
 
 		});
