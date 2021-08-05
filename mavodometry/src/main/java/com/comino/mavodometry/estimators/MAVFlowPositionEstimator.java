731f833de2331fffe2360944eb2a60a507f93018
ecmnet <ecm@gmx.de>
2021-08-05 07:25:12 +0200

    Simple FlowPosition estimator added

---
+++ mavodometry/src/main/java/com/comino/mavodometry/estimators/MAVFlowPositionEstimator.java
@@ -0,0 +1,104 @@
+package com.comino.mavodometry.estimators;
+
+//refer to : https://github.com/PX4/PX4-Autopilot/blob/master/src/modules/local_position_estimator/sensors/flow.cpp
+
+
+import com.comino.mavcom.control.IMAVMSPController;
+import com.comino.mavcom.core.ControlModule;
+import com.comino.mavcom.flow.MessageBus;
+import com.comino.mavcom.flow.ModelSubscriber;
+import com.comino.mavcom.model.DataModel;
+import com.comino.mavcom.model.segment.Raw;
+import com.comino.mavcom.model.segment.Status;
+import com.comino.mavcom.utils.MSP3DUtils;
+
+import georegression.geometry.GeometryMath_F64;
+import georegression.struct.point.Vector3D_F64;
+import georegression.struct.se.Se3_F64;
+
+public class MAVFlowPositionEstimator extends ControlModule  {
+	
+	private static final float FLOWSCALE = 1.0f;
+	
+	private Vector3D_F64     flow       = new Vector3D_F64();
+	private Vector3D_F64     gyro       = new Vector3D_F64();
+	private Vector3D_F64     vel_body   = new Vector3D_F64();
+	private Vector3D_F64     vel_ned    = new Vector3D_F64();
+	private Vector3D_F64     pos_ned    = new Vector3D_F64();
+	
+	private Se3_F64          to_ned     = new Se3_F64();
+	
+	private long tms = 0;
+	
+	private DataModel 	model;
+
+	public MAVFlowPositionEstimator(IMAVMSPController control) {
+		super(control);
+		this.model = control.getCurrentModel();
+	
+		
+		ModelSubscriber<Raw> flow_subscriber = new ModelSubscriber<Raw>(Raw.class,(n) -> {
+			if(n.fq > 0)
+			   estimate();
+		});
+
+		MessageBus.getInstance().subscribe(flow_subscriber);
+		
+	}
+	
+	public boolean estimate() {
+		
+		float dt_int = model.raw.ius * 1e-6f;
+		float dt_sec = ( System.currentTimeMillis() - tms) / 1000f;
+		tms = System.currentTimeMillis();
+		
+		if(dt_int > 0.5 || dt_int < 1e-6f || dt_sec > 0.5)
+			return false;
+		
+		if (Math.abs(model.attitude.p) > 0.5f || Math.abs(model.attitude.r) > 0.5f)
+			return false;
+		
+		if(!model.sys.isStatus(Status.MSP_ARMED)) {
+			reset();
+			return false;
+		}
+		
+		MSP3DUtils.convertModelRotationToSe3_F64(model, to_ned);
+		
+		flow.set(model.raw.fX * FLOWSCALE, model.raw.fY * FLOWSCALE,0);
+		gyro.set(model.raw.fgX,model.raw.fgY,0);
+		
+		float d = model.hud.ar * (float)Math.cos(model.attitude.p) * (float)Math.cos(model.attitude.r);
+		
+		vel_body.set( ( (flow.y - gyro.y) * d ) / dt_int,
+				      (-(flow.x - gyro.x) * d ) / dt_int,
+				      0);
+		
+		GeometryMath_F64.mult(to_ned.R, vel_body,vel_ned);
+		
+		vel_ned.scale(dt_sec);
+		pos_ned.plusIP(vel_ned);
+		
+		model.vision.gx = (float)pos_ned.x;
+		model.vision.gy = (float)pos_ned.y;
+		model.vision.gz = 0f;
+		
+		return true;
+		
+	}
+	
+	public void reset() {
+		pos_ned.set(model.state.l_x, model.state.l_y,model.state.l_z);
+		vel_ned.set(model.state.l_vx, model.state.l_vy,model.state.l_vz);
+		tms = 0;
+	}
+	
+	public Vector3D_F64 getVeocity() {
+		return vel_ned;
+	}
+	
+	public Vector3D_F64 getPosition() {
+		return pos_ned;
+	}
+
+}
