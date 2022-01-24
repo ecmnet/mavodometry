a9c3dd7abab92707a56bc9181d068765a82c66ba
ecmnet <ecm@gmx.de>
2022-01-24 11:22:55 +0100

    MSP Vision simulation enhanced

--- mavodometry/src/main/java/com/comino/mavodometry/estimators/position/MAVSITLPositionEstimator.java
+++ mavodometry/src/main/java/com/comino/mavodometry/estimators/position/MAVSITLPositionEstimator.java
@@ -1,20 +1,25 @@
 package com.comino.mavodometry.estimators.position;
 
+import org.mavlink.messages.MAV_SEVERITY;
 import org.mavlink.messages.MSP_CMD;
 import org.mavlink.messages.MSP_COMPONENT_CTRL;
 import org.mavlink.messages.lquac.msg_msp_command;
 import org.mavlink.messages.lquac.msg_msp_vision;
 import org.mavlink.messages.lquac.msg_odometry;
+import org.mavlink.messages.lquac.msg_set_gps_global_origin;
 
 import com.comino.mavcom.config.MSPParams;
 import com.comino.mavcom.control.IMAVMSPController;
+import com.comino.mavcom.log.MSPLogger;
 import com.comino.mavcom.mavlink.IMAVLinkListener;
 import com.comino.mavcom.model.DataModel;
 import com.comino.mavcom.model.segment.Status;
 import com.comino.mavcom.model.segment.Vision;
+import com.comino.mavcom.utils.MSP3DComplementaryFilter;
 import com.comino.mavcom.utils.MSP3DUtils;
 import com.comino.mavodometry.estimators.MAVAbstractEstimator;
 import com.comino.mavodometry.estimators.drift.MAVDriftSpeedEstimator;
+import com.comino.mavodometry.utils.TimeHysteris;
 import com.comino.mavutils.workqueue.WorkQueue;
 
 import georegression.geometry.GeometryMath_F64;
@@ -28,8 +33,11 @@ public class MAVSITLPositionEstimator extends MAVAbstractEstimator implements IM
 	private final WorkQueue wq = WorkQueue.getInstance();
 
 	private final DataModel 	     model;
+	private final Vector3D_F64       pos_ned    = new Vector3D_F64();
+	
 	private final Vector3D_F64       vel_ned    = new Vector3D_F64();
 	private final Vector3D_F64       vel_body   = new Vector3D_F64();
+	
 	private final Vector3D_F64       vpo_body   = new Vector3D_F64();
 	private final Se3_F64            to_ned     = new Se3_F64();
 
@@ -37,7 +45,8 @@ public class MAVSITLPositionEstimator extends MAVAbstractEstimator implements IM
 	private boolean                  is_running = false;
 
 	private long                     tms = 0;
-	private MAVDriftSpeedEstimator drift = new MAVDriftSpeedEstimator(0.1,0.2,30);
+
+	
 
 	public MAVSITLPositionEstimator(IMAVMSPController control) {
 		super(control);
@@ -46,17 +55,54 @@ public class MAVSITLPositionEstimator extends MAVAbstractEstimator implements IM
 
 		control.sendMAVLinkMessage(msg);
 
+		control.registerListener(msg_msp_command.class, new IMAVLinkListener() {
+			@Override
+			public void received(Object o) {
+				msg_msp_command cmd = (msg_msp_command)o;
+				switch(cmd.command) {
+				case MSP_CMD.MSP_CMD_SET_HOMEPOS:
+					setGlobalOrigin(cmd.param1 / 1e7f, cmd.param2 / 1e7f, cmd.param3 / 1e3f );
+					break;
+				}
+			}
+		});
+
+
 		wq.addCyclicTask("LP", 30, () -> {
-			
+
 			if(is_running) {
 				msg.fps     = 1000f / (System.currentTimeMillis() - tms);
 				tms = System.currentTimeMillis();
-				control.sendMAVLinkMessage(msg);
+			
 				
-				// Drift estimation
-				if(drift.estimate(vel_body, vpo_body)) {
-					model.debug.set(drift.get());
-				}
+				// Convert body velocities to NED
+				MSP3DUtils.convertModelToSe3_F64(model, to_ned);
+				GeometryMath_F64.mult(to_ned.R, vel_body,vel_ned);
+				
+				msg.x =  (float)pos_ned.x;
+				msg.y =  (float)pos_ned.y;
+				msg.z =  (float)pos_ned.z;
+
+				msg.vx = (float)vel_ned.x;
+				msg.vy = (float)vel_ned.y;
+				msg.vz = (float)vel_ned.z;
+				
+				
+
+				// Simulate drift estimation
+				vpo_body.setTo(msg.vx+0.03+Math.random()/1000.0, msg.vy+Math.random()/1000.0,msg.vz);
+
+				msg.p  = model.attitude.p;
+				msg.r  = model.attitude.r;
+				msg.h  = model.attitude.y;
+
+				msg.quality = 100;
+				msg.errors  = 0;
+				msg.tms     = DataModel.getSynchronizedPX4Time_us();
+				msg.flags   = model.vision.flags;
+				
+				control.sendMAVLinkMessage(msg);
+
 
 			}
 		});
@@ -84,30 +130,11 @@ public class MAVSITLPositionEstimator extends MAVAbstractEstimator implements IM
 		// Just send msg_odometry as msp_vision message
 		msg_odometry odometry = (msg_odometry)o;
 
-		msg.x =  odometry.x;
-		msg.y =  odometry.y;
-		msg.z =  odometry.z;
-		
-		// Convert body velocities to NED
-		MSP3DUtils.convertModelToSe3_F64(model, to_ned);
+		pos_ned.setTo(odometry.x,odometry.y,odometry.z);
 		vel_body.setTo(odometry.vx,odometry.vy,odometry.vz);
-		GeometryMath_F64.mult(to_ned.R, vel_body,vel_ned);
 		
-		msg.vx = (float)vel_ned.x;
-		msg.vy = (float)vel_ned.y;
-		msg.vz = (float)vel_ned.z;
 		
-		// Simulate drift estimation
-		vpo_body.setTo(msg.vx+0.03+Math.random()/1000.0, msg.vy+Math.random()/1000.0,msg.vz);
 
-		msg.p  = model.attitude.p;
-		msg.r  = model.attitude.r;
-		msg.h  = model.attitude.y;
-
-		msg.quality = 100;
-		msg.errors  = 0;
-		msg.tms     = odometry.time_usec;
-		msg.flags   = model.vision.flags;
 
 	}
 
@@ -115,25 +142,50 @@ public class MAVSITLPositionEstimator extends MAVAbstractEstimator implements IM
 
 
 	public void reset() {
-		drift.reset();
+
 	}
 
 
 	@Override
 	public void enableStream(boolean enable) {
-		
+
 
 	}
 
 	@Override
 	public void start() throws Exception {
-		drift.reset();
+
 
 	}
 
 	@Override
 	public void stop() {
-		drift.reset();
+
 	}
+
+	// TODO: Move to commander
+	private void setGlobalOrigin(double lat, double lon, double altitude) {
+
+		if(model.sys.isSensorAvailable(Status.MSP_GPS_AVAILABILITY) || model.sys.isStatus(Status.MSP_GPOS_VALID))
+			return;
+
+		// Note: In SITL Set global origin causes BARO failure 
+		// TODO: To be investigated in PX4
+
+		//
+		//			msg_set_gps_global_origin gor = new msg_set_gps_global_origin(1,1);
+		//			gor.target_system = 1;
+		//			gor.latitude = (long)(lat * 1e7);
+		//			gor.longitude = (long)(lon * 1e7);
+		//			gor.altitude = (int)(altitude * 1000);
+		//			gor.time_usec = DataModel.getSynchronizedPX4Time_us();
+		//
+		//			control.sendMAVLinkMessage(gor);
+		//
+		//			MSPLogger.getInstance().writeLocalMsg("[msp] Setting reference position",
+		//					MAV_SEVERITY.MAV_SEVERITY_INFO);
+
+	}
+
 
 }
