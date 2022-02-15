dc644883c6877a0ae3b772e5d30dac34ff748c1d
ecmnet <ecm@gmx.de>
2022-02-15 14:15:49 +0100

    CleanUp

--- mavodometry/src/main/java/com/comino/mavodometry/libwebcam/StreamWebcam.java
+++ mavodometry/src/main/java/com/comino/mavodometry/libwebcam/StreamWebcam.java
@@ -1,172 +1,172 @@
 /*
- * Copyright (c) 2021, Peter Abeles. All Rights Reserved.
- *
- * This file is part of BoofCV (http://boofcv.org).
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.comino.mavodometry.libwebcam;
-
-import org.bytedeco.javacv.Frame;
-import org.bytedeco.javacv.FrameGrabber;
-import org.bytedeco.javacv.FrameGrabber.Exception;
-import org.bytedeco.javacv.OpenCVFrameGrabber;
-import org.ddogleg.struct.DogArray_I8;
-
-import boofcv.io.image.SimpleImageSequence;
-import boofcv.io.wrapper.WebcamInterface;
-import boofcv.javacv.ConvertOpenCvFrame;
-import boofcv.struct.image.ImageBase;
-import boofcv.struct.image.ImageType;
-
-/**
- * Implementation of {@link WebcamInterface} for OpenCV.
- *
- * @author Peter Abeles
- */
-public class StreamWebcam  {
-	
-	private OpenCVFrameGrabber grabber = null;
-	
-	public <T extends ImageBase<T>> SimpleImageSequence<T>
-	open(String device, int width, int height, float rate, ImageType<T> imageType)
-	{
-		if( device != null ) {
-			try {
-				int which = Integer.parseInt(device);
-				grabber = new OpenCVFrameGrabber(which);
-			}catch (NumberFormatException ignore) {
-				grabber = new OpenCVFrameGrabber(0);
-			}
-			if( grabber == null ) {
-				throw new RuntimeException("Can't find webcam with ID or name at "+device);
-			}
-		} else {
-			grabber = new OpenCVFrameGrabber(0);
-		}
-		grabber.setImageWidth(width);
-		grabber.setImageHeight(height);
-		grabber.setFrameRate(rate);
-		return new SimpleSequence<>(grabber, imageType);
-	}
-	
-	public OpenCVFrameGrabber getGrabber() {
-		return grabber;
-	}
-	
-	public void stop() {
-		try {
-			grabber.stop();
-			grabber.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static class SimpleSequence<T extends ImageBase<T>> implements SimpleImageSequence<T> {
-
-		OpenCVFrameGrabber grabber;
-		int width,height;
-
-		T output;
-
-		int frameNumber;
-
-		DogArray_I8 work = new DogArray_I8();
-
-		boolean bgr_to_rgb;
-
-		public SimpleSequence(OpenCVFrameGrabber grabber, ImageType<T> imageType) {
-			this.grabber = grabber;
-
-			try {
-				this.grabber.start();
-			} catch (FrameGrabber.Exception e) {
-				throw new RuntimeException(e);
-			}
-
-			bgr_to_rgb = grabber.getPixelFormat() == 1;
-
-			width = grabber.getImageWidth();
-			height = grabber.getImageHeight();
-			output = imageType.createImage(width,height);
-		}
-
-		@Override
-		public int getWidth() {
-			return width;
-		}
-
-		@Override
-		public int getHeight() {
-			return height;
-		}
-
-		@Override
-		public boolean hasNext() {
-			try {
-				Frame image = grabber.grab();
-				frameNumber = grabber.getFrameNumber();
-				ConvertOpenCvFrame.convert(image,output,bgr_to_rgb,work);
-				return true;
-			} catch (FrameGrabber.Exception e) {
-				return false;
-			}
-		}
-
-		@Override
-		public T next() {
-			return output;
-		}
-
-		@Override
-		public T getImage() {
-			return output;
-		}
-
-		@Override
-		public <InternalImage> InternalImage getGuiImage() {
-			return null;
-		}
-
-		@Override
-		public void close() {
-			try {
-				grabber.stop();
-			} catch (FrameGrabber.Exception e) {
-				throw new RuntimeException(e);
-			}
-		}
-
-		@Override
-		public int getFrameNumber() {
-			return frameNumber;
-		}
-
-		@Override
-		public void setLoop(boolean loop) {
-
-		}
-
-		@Override
-		public ImageType<T> getImageType() {
-			return output.getImageType();
-		}
-
-		@Override
-		public void reset() {
-
-		}
-	}
-}
+// * Copyright (c) 2021, Peter Abeles. All Rights Reserved.
+// *
+// * This file is part of BoofCV (http://boofcv.org).
+// *
+// * Licensed under the Apache License, Version 2.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * You may obtain a copy of the License at
+// *
+// *   http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// */
+//
+//package com.comino.mavodometry.libwebcam;
+//
+//import org.bytedeco.javacv.Frame;
+//import org.bytedeco.javacv.FrameGrabber;
+//import org.bytedeco.javacv.FrameGrabber.Exception;
+//import org.bytedeco.javacv.OpenCVFrameGrabber;
+//import org.ddogleg.struct.DogArray_I8;
+//
+//import boofcv.io.image.SimpleImageSequence;
+//import boofcv.io.wrapper.WebcamInterface;
+//import boofcv.javacv.ConvertOpenCvFrame;
+//import boofcv.struct.image.ImageBase;
+//import boofcv.struct.image.ImageType;
+//
+///**
+// * Implementation of {@link WebcamInterface} for OpenCV.
+// *
+// * @author Peter Abeles
+// */
+//public class StreamWebcam  {
+//	
+//	private OpenCVFrameGrabber grabber = null;
+//	
+//	public <T extends ImageBase<T>> SimpleImageSequence<T>
+//	open(String device, int width, int height, float rate, ImageType<T> imageType)
+//	{
+//		if( device != null ) {
+//			try {
+//				int which = Integer.parseInt(device);
+//				grabber = new OpenCVFrameGrabber(which);
+//			}catch (NumberFormatException ignore) {
+//				grabber = new OpenCVFrameGrabber(0);
+//			}
+//			if( grabber == null ) {
+//				throw new RuntimeException("Can't find webcam with ID or name at "+device);
+//			}
+//		} else {
+//			grabber = new OpenCVFrameGrabber(0);
+//		}
+//		grabber.setImageWidth(width);
+//		grabber.setImageHeight(height);
+//		grabber.setFrameRate(rate);
+//		return new SimpleSequence<>(grabber, imageType);
+//	}
+//	
+//	public OpenCVFrameGrabber getGrabber() {
+//		return grabber;
+//	}
+//	
+//	public void stop() {
+//		try {
+//			grabber.stop();
+//			grabber.close();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//
+//	public static class SimpleSequence<T extends ImageBase<T>> implements SimpleImageSequence<T> {
+//
+//		OpenCVFrameGrabber grabber;
+//		int width,height;
+//
+//		T output;
+//
+//		int frameNumber;
+//
+//		DogArray_I8 work = new DogArray_I8();
+//
+//		boolean bgr_to_rgb;
+//
+//		public SimpleSequence(OpenCVFrameGrabber grabber, ImageType<T> imageType) {
+//			this.grabber = grabber;
+//
+//			try {
+//				this.grabber.start();
+//			} catch (FrameGrabber.Exception e) {
+//				throw new RuntimeException(e);
+//			}
+//
+//			bgr_to_rgb = grabber.getPixelFormat() == 1;
+//
+//			width = grabber.getImageWidth();
+//			height = grabber.getImageHeight();
+//			output = imageType.createImage(width,height);
+//		}
+//
+//		@Override
+//		public int getWidth() {
+//			return width;
+//		}
+//
+//		@Override
+//		public int getHeight() {
+//			return height;
+//		}
+//
+//		@Override
+//		public boolean hasNext() {
+//			try {
+//				Frame image = grabber.grab();
+//				frameNumber = grabber.getFrameNumber();
+//				ConvertOpenCvFrame.convert(image,output,bgr_to_rgb,work);
+//				return true;
+//			} catch (FrameGrabber.Exception e) {
+//				return false;
+//			}
+//		}
+//
+//		@Override
+//		public T next() {
+//			return output;
+//		}
+//
+//		@Override
+//		public T getImage() {
+//			return output;
+//		}
+//
+//		@Override
+//		public <InternalImage> InternalImage getGuiImage() {
+//			return null;
+//		}
+//
+//		@Override
+//		public void close() {
+//			try {
+//				grabber.stop();
+//			} catch (FrameGrabber.Exception e) {
+//				throw new RuntimeException(e);
+//			}
+//		}
+//
+//		@Override
+//		public int getFrameNumber() {
+//			return frameNumber;
+//		}
+//
+//		@Override
+//		public void setLoop(boolean loop) {
+//
+//		}
+//
+//		@Override
+//		public ImageType<T> getImageType() {
+//			return output.getImageType();
+//		}
+//
+//		@Override
+//		public void reset() {
+//
+//		}
+//	}
+//}
