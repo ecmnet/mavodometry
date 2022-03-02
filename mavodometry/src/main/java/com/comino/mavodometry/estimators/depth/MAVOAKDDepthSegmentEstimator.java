0767667551516bca5e4c3c56d3b25c880d853d1e
ecmnet <ecm@gmx.de>
2022-03-02 12:04:52 +0100

    Copyright added

--- mavodometry/src/main/java/com/comino/mavodometry/estimators/depth/MAVOAKDDepthSegmentEstimator.java
+++ mavodometry/src/main/java/com/comino/mavodometry/estimators/depth/MAVOAKDDepthSegmentEstimator.java
@@ -1,5 +1,37 @@
 package com.comino.mavodometry.estimators.depth;
 
+/****************************************************************************
+*
+*   Copyright (c) 2022 Eike Mansfeld ecm@gmx.de. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* 1. Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in
+*    the documentation and/or other materials provided with the
+*    distribution.
+* 3. Neither the name of the copyright holder nor the names of its
+*    contributors may be used to endorse or promote products derived
+*    from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+* AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+* ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*
+****************************************************************************/
 
 import static boofcv.factory.distort.LensDistortionFactory.narrow;
 
@@ -160,9 +192,13 @@ public class MAVOAKDDepthSegmentEstimator extends MAVAbstractEstimator  {
 	 * Processing depth data at 10 Hz.
 	 */
 	private class DepthHandler implements Runnable {
+		
+		private final static int DEPTH_SEG_W_2 = DEPTH_SEG_W/2;
+		private final static int DEPTH_SEG_H_2 = DEPTH_SEG_H/2;
 
 		private final Point3D_F64 ned_pt_n = new Point3D_F64();
-
+		private final Point2D_F64 norm     = new Point2D_F64();
+		
 		@Override
 		public void run() {
 
@@ -242,11 +278,11 @@ public class MAVOAKDDepthSegmentEstimator extends MAVAbstractEstimator  {
 		}
 
 		// Return 3D point of depth segment in body frame
-		final Point2D_F64 norm = new Point2D_F64();
+	
 		private void getSegmentPositionBody(int x, int y, GrayF32 seg, Point2D3D p) {
 
-			p.observation.x = x*DEPTH_SEG_W+DEPTH_SEG_W/2;
-			p.observation.y = y*DEPTH_SEG_H+DEPTH_SEG_H/2;
+			p.observation.x = x*DEPTH_SEG_W+DEPTH_SEG_W_2;
+			p.observation.y = y*DEPTH_SEG_H+DEPTH_SEG_H_2;
 
 			p2n.compute(p.observation.x,p.observation.y,norm);
 
