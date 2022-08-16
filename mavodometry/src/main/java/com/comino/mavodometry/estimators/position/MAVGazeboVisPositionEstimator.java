2fa90c1e8b8a35a49a7c3be25d079745f022330b
ecmnet <ecm@gmx.de>
2022-08-16 14:24:15 +0200

    Add groundtruth altitude (local)

--- mavodometry/src/main/java/com/comino/mavodometry/estimators/position/MAVGazeboVisPositionEstimator.java
+++ mavodometry/src/main/java/com/comino/mavodometry/estimators/position/MAVGazeboVisPositionEstimator.java
@@ -57,7 +57,7 @@ public class MAVGazeboVisPositionEstimator extends MAVAbstractEstimator  {
 	private long                    tms_reset       = 0;
 	private int                     reset_count     = 0;
 
-	private final float[]           groundtruth     = new float[2];
+	private final float[]           groundtruth     = new float[3];
 	private float                   cov_velocity    = STD_VEL_COVARIANCE;
 
 	private final StreamGazeboVision vis;
@@ -118,10 +118,12 @@ public class MAVGazeboVisPositionEstimator extends MAVAbstractEstimator  {
 
 		groundtruth[0] = Float.NaN;
 		groundtruth[1] = Float.NaN;
+		groundtruth[2] = Float.NaN;
 
 		vis.registerCallback((tms,lat,lon,alt) -> {
 			if(MSPMathUtils.is_projection_initialized()) {
 				MSPMathUtils.map_projection_project(lat, lon, groundtruth);
+				groundtruth[2] = -((float)alt - model.home_state.g_alt);
 			}	
 		});
 
@@ -190,26 +192,26 @@ public class MAVGazeboVisPositionEstimator extends MAVAbstractEstimator  {
 			ned_s.setTo(s);
 			
 			
-			double noisex = (double)Math.random()+0.5;
-			double noisey = (double)Math.random()+0.5;
-			double noisez = (double)Math.random()+0.5;
+//			double noisex = (double)Math.random()+0.5;
+//			double noisey = (double)Math.random()+0.5;
+//			double noisez = (double)Math.random()+0.5;
 
-			// Simulate a 5 secs vision glitch between 20 and 24 secs arming
-			if(model.sys.t_armed_ms > 20000 && model.sys.t_armed_ms < 25000) {
-				ned_s.T.x *= noisex; ned_s.T.y *= noisey; ned_s.T.z *= noisez;
-			}
-
-			// Simulate a 0.5secs vision glitch between 30 and 31 secs arming for 100ms
-			if(model.sys.t_armed_ms > 30000 && model.sys.t_armed_ms <30500) {
-				ned_s.T.x *= noisex * 5 ; ned_s.T.y *= noisey * 5; ned_s.T.z *= noisez;
-			}
-
-			// Simulate a 10secs vision glitch between 40 and 60 secs arming
-			if(model.sys.t_armed_ms > 40000 && model.sys.t_armed_ms < 60000) {
-				double tmp = ned_s.T.x;
-				ned_s.T.x = ned_s.T.y*noisex; ned_s.T.y = tmp * noisey; ned_s.T.z *= noisez;
-				ned.T.x   *= noisex; ned.T.y   *= noisey; ned.T.z   *= noisez;
-			}
+//			// Simulate a 5 secs vision glitch between 20 and 24 secs arming
+//			if(model.sys.t_armed_ms > 20000 && model.sys.t_armed_ms < 25000) {
+//				ned_s.T.x *= noisex; ned_s.T.y *= noisey; ned_s.T.z *= noisez;
+//			}
+//
+//			// Simulate a 0.5secs vision glitch between 30 and 31 secs arming for 100ms
+//			if(model.sys.t_armed_ms > 30000 && model.sys.t_armed_ms <30500) {
+//				ned_s.T.x *= noisex * 5 ; ned_s.T.y *= noisey * 5; ned_s.T.z *= noisez;
+//			}
+//
+//			// Simulate a 10secs vision glitch between 40 and 60 secs arming
+//			if(model.sys.t_armed_ms > 40000 && model.sys.t_armed_ms < 60000) {
+//				double tmp = ned_s.T.x;
+//				ned_s.T.x = ned_s.T.y*noisex; ned_s.T.y = tmp * noisey; ned_s.T.z *= noisez;
+//				ned.T.x   *= noisex; ned.T.y   *= noisey; ned.T.z   *= noisez;
+//			}
 
 			att_euler.setFromMatrix(ned.R).add(att_offset);			
 			model.vision.setStatus(Vision.ERROR, false);
@@ -228,7 +230,7 @@ public class MAVGazeboVisPositionEstimator extends MAVAbstractEstimator  {
 
 				publishMSPFlags(tms);
 				
-				System.err.println(model.sys.t_armed_ms+": "+error_count);
+				
 
 				if(++error_count > MAX_ERRORS) {
 					init("EKF2 TestRatio");
@@ -248,9 +250,7 @@ public class MAVGazeboVisPositionEstimator extends MAVAbstractEstimator  {
 			// Filter a covariance for velocity based on test ratio
 			if(Float.isFinite(model.est.velRatio) && model.est.velRatio > 0 && Float.isFinite(cov_velocity))
 				cov_velocity = cov_velocity * 0.98f + model.est.velRatio * 3f *0.02f;
-			model.debug.x = cov_velocity;
-
-
+//			model.debug.x = cov_velocity;
 
 			publishPX4Odometry(ned.T,body_s.T, MAV_FRAME.MAV_FRAME_LOCAL_NED, cov_velocity ,confidence,tms);
 
@@ -396,6 +396,7 @@ public class MAVGazeboVisPositionEstimator extends MAVAbstractEstimator  {
 
 		msg.gx  = gth[0];
 		msg.gy  = gth[1];
+		msg.gz  = gth[2];
 
 		msg.quality = 100;
 		msg.errors  = 0;
